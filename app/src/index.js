f4af036ab51f63b225e13855860a32b9850cbb6d
Mark Rivera <Mriver1991@gmail.com>
2020-05-24 11:56:13 -0400

    added persistence of job display data into local storage

--- app/src/index.js
+++ app/src/index.js
@@ -4,13 +4,20 @@ import "./index.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
 
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import { reducer } from "./reducers";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
+import persistState from "redux-localstorage";
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  compose(
+    composeWithDevTools(applyMiddleware(thunk)),
+    persistState(undefined, { key: "store" })
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
